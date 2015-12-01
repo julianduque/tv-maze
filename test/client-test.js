8fd7945cf62b3cf7073c2e533b290f39bc96712a
Julian Duque <julianduquej@gmail.com>
2015-11-30 21:04:19 -0500

    Add shows function to client
    - Use nock for mocking http requests

--- test/client-test.js
+++ test/client-test.js
@@ -1,6 +1,9 @@
 var test = require('tape')
+var nock = require('nock')
 var tvmaze = require('../')
 var Client = require('../lib/client')
+
+var endpoint = 'http://api.tvmaze.test'
 
 test('should create a client', function (t) {
   t.ok(tvmaze.createClient, 'should exist')
@@ -9,4 +12,19 @@ test('should create a client', function (t) {
   var client = tvmaze.createClient()
   t.ok(client instanceof Client, 'should be instance of Client')
   t.end()
+})
+
+test('should list shows', function (t) {
+  var client = tvmaze.createClient({ endpoint: endpoint })
+  t.equals(typeof client.shows, 'function', 'should be a function')
+
+  nock(endpoint)
+    .get('/shows')
+    .reply(200, [])
+
+  client.shows(function (err, shows) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(shows), 'should be an array')
+    t.end()
+  })
 })
