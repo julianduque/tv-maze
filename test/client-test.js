e5f313135ed92f2aaa8a9daffe80ebcfc94e87d8
Julian Duque <julianduquej@gmail.com>
2015-11-30 21:17:18 -0500

    Add search function

--- test/client-test.js
+++ test/client-test.js
@@ -28,3 +28,20 @@ test('should list shows', function (t) {
     t.end()
   })
 })
+
+test('should search shows', function (t) {
+  var client = tvmaze.createClient({ endpoint: endpoint })
+  t.equals(typeof client.search, 'function', 'should be a function')
+
+  nock(endpoint)
+    .get('/search/shows')
+    .query({ q: 'limitless' })
+    .reply(200, [{ name: 'Limitless' }])
+
+  client.search('limitless', function (err, shows) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(shows), 'should be an array')
+    t.equals(shows[0].name, 'Limitless', 'should retrieve a show name')
+    t.end()
+  })
+})
