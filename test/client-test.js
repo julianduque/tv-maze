6cfcdbbdcd3d504ab414251c5dc521a464d0d9de
Alejo Moreno <tecksonic@gmail.com>
2016-05-06 18:47:22 -0500

    shows function updated

--- test/client-test.js
+++ test/client-test.js
@@ -3,9 +3,9 @@ var nock = require('nock')
 var tvmaze = require('../')
 var Client = require('../lib/client')
 
-var endpoint = 'http://api.tvmaze.com'
+var endpoint = 'http://api.tvmaze.test'
 
-test('should create a client', function(t) {
+test('should create a client', function (t) {
     t.ok(tvmaze.createClient, 'should exist')
     t.equals(typeof tvmaze.createClient, 'function', 'should be a function')
 
@@ -14,19 +14,19 @@ test('should create a client', function(t) {
     t.end()
 })
 
-test('should fail with unknown endpoint', function(t) {
+test('should fail with unknown endpoint', function (t) {
     var client = tvmaze.createClient({ endpoint: endpoint })
     nock(endpoint)
         .get('/foo')
         .reply(404)
 
-    client._request('/foo', 'GET', null, function(err, body) {
+    client._request('/foo', 'GET', null, function (err, body) {
         t.ok(err, 'request failed')
         t.end()
     })
 })
 
-test('should fetch a show by id', function(t) {
+test('should fetch a show by id', function (t) {
     var client = tvmaze.createClient({ endpoint: endpoint })
     t.equals(typeof client.show, 'function', 'should be a function')
 
@@ -34,7 +34,7 @@ test('should fetch a show by id', function(t) {
         .get('/shows/2473')
         .reply(200, { id: 2473, name: 'Limitless' })
 
-    client.show(2473, function(err, show) {
+    client.show(2473, function (err, show) {
         t.error(err, 'should not be an error')
         t.ok(show, 'should exist')
         t.equals(show.id, 2473)
@@ -43,7 +43,7 @@ test('should fetch a show by id', function(t) {
     })
 })
 
-test('should list shows', function(t) {
+test('should list shows of first page', function (t) {
     var client = tvmaze.createClient({ endpoint: endpoint })
     t.equals(typeof client.shows, 'function', 'should be a function')
 
@@ -51,14 +51,34 @@ test('should list shows', function(t) {
         .get('/shows')
         .reply(200, [])
 
-    client.shows(function(err, shows) {
+    client.shows(null, function (err, shows) {
         t.error(err, 'should not be an error')
         t.ok(Array.isArray(shows), 'should be an array')
         t.end()
     })
 })
 
-test('should search shows', function(t) {
+test('should list shows of n page', function (t) {
+    var client = tvmaze.createClient({ endpoint: endpoint })
+    t.equals(typeof client.shows, 'function', 'should be a function')
+
+    nock(endpoint)
+        .get('/shows?page=1')
+        .reply(200, [])
+
+    var pagination = {
+        q: 'page?',
+        page: 1
+    }
+
+    client.shows(pagination, function (err, shows) {
+        t.error(err, 'should not be an error')
+        t.ok(Array.isArray(shows), 'should be an array')
+        t.end()
+    })
+})
+
+test('should search shows', function (t) {
     var client = tvmaze.createClient({ endpoint: endpoint })
     t.equals(typeof client.search, 'function', 'should be a function')
 
@@ -67,7 +87,7 @@ test('should search shows', function(t) {
         .query({ q: 'limitless' })
         .reply(200, [{ name: 'Limitless' }])
 
-    client.search('limitless', function(err, shows) {
+    client.search('limitless', function (err, shows) {
         t.error(err, 'should not be an error')
         t.ok(Array.isArray(shows), 'should be an array')
         t.equals(shows[0].name, 'Limitless', 'should retrieve a show name')
@@ -75,7 +95,7 @@ test('should search shows', function(t) {
     })
 })
 
-test('should fail if not query is passed', function(t) {
+test('should fail if not query is passed', function (t) {
     var client = tvmaze.createClient({ endpoint: endpoint })
 
     nock(endpoint)
@@ -87,14 +107,14 @@ test('should fail if not query is passed', function(t) {
             status: 400
         })
 
-    client._request('/search/shows', 'GET', null, function(err, res) {
+    client._request('/search/shows', 'GET', null, function (err, res) {
         t.ok(err, 'bad request error')
         t.notOk(res, 'reply should be null')
         t.end()
     })
 })
 
-test('should show the cast of a show', function(t) {
+test('should show the cast of a show', function (t) {
 
     var client = tvmaze.createClient({ endpoint: endpoint })
     t.equals(typeof client.cast, 'function', 'Should be a function')
@@ -103,7 +123,7 @@ test('should show the cast of a show', function(t) {
         .get('/shows/175/cast')
         .reply(200, [{ person: { name: 'Robin Wright' } }])
 
-    client.cast(175, function(err, cast) {
+    client.cast(175, function (err, cast) {
         t.error(err, 'should not be an error')
         t.ok(Array.isArray(cast), 'should be an array ')
         t.equals(cast[0].person.name, 'Robin Wright', 'should retrive a name')
@@ -111,7 +131,7 @@ test('should show the cast of a show', function(t) {
     })
 })
 
-test('should show the episodes of a show', function(t) {
+test('should show the episodes of a show', function (t) {
     var client = tvmaze.createClient({ endpoint: endpoint })
     t.equals(typeof client.episodes, 'function', 'should be a function')
 
@@ -119,7 +139,7 @@ test('should show the episodes of a show', function(t) {
         .get('/shows/175/episodes')
         .reply(200, [{ name: 'Pilot' }])
 
-    client.episodes(175, function(err, episodes) {
+    client.episodes(175, function (err, episodes) {
         t.error(err, 'should not be an error')
         t.ok(Array.isArray(episodes), 'should be an array')
         t.equals(episodes[0].name, 'Pilot', 'should retrieve a name')
